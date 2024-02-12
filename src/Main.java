9ccc1c00d5fb7b8f2e244030535e51b20d5c98e1
akhramova <xaa2128@mail.ru>
2024-02-12 17:18:57 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,23 @@ public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
     public static void main(String[] args) {
+        // почему-то печатает только два раза: в начале и в конце программы
+        Thread thread = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Optional<Integer> max = sizeToFreq.keySet().stream().max(Comparator.comparingInt(sizeToFreq::get));
+                    System.out.println("Самое частое количество повторений " + max.get() +
+                            " (встретилось " + sizeToFreq.get(max.get()) + " раз)");
+                }
+            }
+        });
+        thread.start();;
+
         for (int i = 0; i < 1000; i++) {
             new Thread(() -> {
                 synchronized (sizeToFreq) {
@@ -16,12 +33,15 @@ public class Main {
                     } else {
                         sizeToFreq.put(count, 1);
                     }
+                    sizeToFreq.notify();
                 }
             }).start();
         }
+
         while (true) { //не поняла как по-другому дождаться когда все потоки закончат работу
             synchronized (sizeToFreq) {
                 if (sizeToFreq.values().stream().reduce(0, (a, b) -> a + b) == 1000) {
+                    thread.interrupt();
                     Optional<Integer> max = sizeToFreq.keySet().stream().max(Comparator.comparingInt(sizeToFreq::get));
                     System.out.println("Самое частое количество повторений " + max.get() +
                             " (встретилось " + sizeToFreq.get(max.get()) + " раз)\n" +
